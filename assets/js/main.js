c10390047be974c322e95e604a83e3462ac4db72
mrg-land <glandcomputingacademy@gmail.com>
2024-10-09 15:20:57 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Technician", "Designer", "Frontend Developer"],
+  strings : ["Technician", "Designer", "Web Developer"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
